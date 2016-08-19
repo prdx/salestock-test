ac3d66969b9290b4abea6661c9e757f84ce70360
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-08-20 01:28:29 +0800

    Add product controller test.

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ProductController, type: :controller do
-
+  context 'All Users' do
+    it 'sends list of products' do
+      get :index
+      expect(response).to be_success
+    end
+  end
 end
