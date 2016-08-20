696a6d22b6fd4f423235d84eb94e5a7102162206
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-08-20 22:31:59 +0800

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819151011) do
+ActiveRecord::Schema.define(version: 20160820120519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,16 @@ ActiveRecord::Schema.define(version: 20160819151011) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string   "status"
+    t.string   "status",                 default: "INITIATED"
     t.integer  "coupon_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.integer  "total_prize",            default: 0
+    t.string   "name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "address"
+    t.integer  "discounted_total_prize", default: 0
     t.index ["coupon_id"], name: "index_orders_on_coupon_id", using: :btree
   end
 
