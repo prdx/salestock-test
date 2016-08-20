fe83f4040bdcfcf6f1a0e1693fc4f84877b54826
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-08-20 22:45:47 +0800

    Add test and validation for product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  validates_presence_of :product_name, :quantity
   has_many :orderlines
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid if product_name is empty' do
+    product = FactoryGirl.build(:product, product_name: nil)
+    expect(product).to_not be_valid
+  end
+
+  it 'is not valid if code is empty' do
+    product = FactoryGirl.build(:product, quantity: nil)
+    expect(product).to_not be_valid
+  end
 end
