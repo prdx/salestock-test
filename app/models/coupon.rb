f39f7a5c78c897686d5c1628b3e276a85e050a15
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-08-20 22:41:48 +0800

    Add test and validation to Coupon.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,4 @@
 class Coupon < ApplicationRecord
+  validates_presence_of :valid_until, :code, :quantity, :discount, :discount_type
   has_many :orders
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid if valid_until is empty' do
+    coupon = FactoryGirl.build(:coupon, valid_until: nil)
+    expect(coupon).to_not be_valid
+  end
+
+  it 'is not valid if code is empty' do
+    coupon = FactoryGirl.build(:coupon, code: nil)
+    expect(coupon).to_not be_valid
+  end
+
+  it 'is not valid if quantity is empty' do
+    coupon = FactoryGirl.build(:coupon, quantity: nil)
+    expect(coupon).to_not be_valid
+  end
+
+  it 'is not valid if discount is empty' do
+    coupon = FactoryGirl.build(:coupon, discount: nil)
+    expect(coupon).to_not be_valid
+  end
+
+  it 'is not valid if discount_type is empty' do
+    coupon = FactoryGirl.build(:coupon, discount_type: nil)
+    expect(coupon).to_not be_valid
+  end
+
 end
