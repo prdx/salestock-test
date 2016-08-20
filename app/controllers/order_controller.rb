8fdbf4cc279b7bc6acaf7fcf196085db1dca2367
Bagus Trihatmaja <astungkara.project@gmail.com>
2016-08-21 00:40:52 +0800

    Implement feature in order controller and add test.

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,44 @@
+class OrderController < ApplicationController
+  def index
+    orders = Order.all
+    render json: orders, status: 200
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      render json: @order, status: :created
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  def add_orderline
+    @orderline = Orderline.new(orderline_params)
+
+    if @orderline.save
+      render json: @orderline, status: :created
+    else
+      render json: @orderline.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :name, 
+      :phone, 
+      :email, 
+      :address, 
+      :status, 
+      :coupon_id
+    )
+  end
+
+  def orderline_params
+    params.require(:orderline).permit(:product_id, :order_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :product
+  resources :order
+  resources :coupon
+
+  scope :order, as: :order do
+    post '/add', to: 'order#add_orderline'
+  end
 end
